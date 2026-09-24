1b3da60d3446ddde80dac7216e7c8a60eff2173c
Namish Mahajan <namishmahajan2005@gmail.com>
2026-09-24 18:15:28 +0530

    Initial Commit

---
+++ test.py
@@ -0,0 +1,63 @@
+from youtube_transcript_api import YouTubeTranscriptApi, TranscriptsDisabled
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+from langchain_google_genai import ChatGoogleGenerativeAI, GoogleGenerativeAIEmbeddings
+from langchain_community.vectorstores import FAISS
+from langchain_core.prompts import PromptTemplate
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+api_key = os.getenv("GEMINI_API_KEY")
+
+# Creating Transcript of the Video
+video_id="WszrfSwMz58"
+try:
+    ytt_api=YouTubeTranscriptApi()
+    transcript_list=ytt_api.fetch(video_id,languages=["en"])
+
+    transcript="".join(chunk.text for chunk in transcript_list)
+    #print(transcript_list)
+    # print(len(transcript))
+except Exception as e:
+    print("Error type: ", type(e).__name__)
+    print("Details: ",e)
+
+# Splitting Text into Chunks
+splitter=RecursiveCharacterTextSplitter(chunk_size=1000,chunk_overlap=200)
+chunks=splitter.create_documents([transcript])
+# print(len(chunks))
+#print(chunks[0])
+
+# Generating Embeddings of Chunks
+embeddings=GoogleGenerativeAIEmbeddings(model="gemini-embedding-2",google_api_key=api_key)
+vector_store=FAISS.from_documents(chunks,embeddings)
+# print(vector_store.index_to_docstore_id)
+
+# Creating the Retriever
+retriever=vector_store.as_retriever(search_type="similarity", search_kwargs={"k":4})
+# print(retriever.invoke("What is maximum path sum"))
+
+# Creating LLM and Prompt Template
+llm=ChatGoogleGenerativeAI(model="gemini-3.7-flash", temperature=0.2)
+
+prompt=PromptTemplate(
+    template="""
+        You are a helpful assistant.
+        Answer ONLY from the provided transcript context.
+        If the context is insufficient, just say you don't know.
+
+        {context}
+        Question: {question}
+    """,
+    input_variables=['context','question']
+)
+
+question="What is maximun path sum?"
+
+retrieved_docs=retriever.invoke(question)
+context_text="\n\n".join(doc.page_content for doc in retrieved_docs)
+final_prompt=prompt.invoke({"context":context_text,"question":question})
+
+answer=llm.invoke(final_prompt)
+print("Answer to your Query: ",answer.text)
